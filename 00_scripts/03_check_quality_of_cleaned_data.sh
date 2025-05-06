28d7f6f5732015810c1f90a4cb09ed544eca2219
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-05-06 13:25:10 +0200

    Update 03_check_quality_of_cleaned_data.sh

--- 00_scripts/03_check_quality_of_cleaned_data.sh
+++ 00_scripts/03_check_quality_of_cleaned_data.sh
@@ -6,8 +6,8 @@
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
 
-WORKING_DIRECTORY=/home/fungi/valormicro/03_cleaned_data
-OUTPUT=/home/fungi/valormicro/04_quality_check
+WORKING_DIRECTORY=/scratch_vol0/fungi/valormicro/03_cleaned_data
+OUTPUT=/scratch_vol0/fungi/valormicro/04_quality_check
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
