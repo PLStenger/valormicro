236ff9a48362803c5930d1a5b372bd73117c6b41
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-05-06 13:27:23 +0200

    Update 06_qiime2_denoise_PE.sh

--- 00_scripts/06_qiime2_denoise_PE.sh
+++ 00_scripts/06_qiime2_denoise_PE.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
  
 
-WORKING_DIRECTORY=/home/fungi/valormicro/05_QIIME2
-OUTPUT=/home/fungi/valormicro/05_QIIME2/visual
+WORKING_DIRECTORY=/scratch_vol0/fungi/valormicro/05_QIIME2
+OUTPUT=/scratch_vol0/fungi/valormicro/05_QIIME2/visual
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
 
-METADATA=/home/fungi/valormicro/98_database_files/sample-metadata.tsv
+METADATA=/scratch_vol0/fungi/valormicro/98_database_files/sample-metadata.tsv
 # negative control sample :
-#NEG_CONTROL=/home/fungi/valormicro/98_database_files/Negative_control_Sample_RepSeq_V4.qza
-NEG_CONTROL=/home/fungi/valormicro/99_contamination
-#NEG_CONTROL=/home/fungi/valormicro/05_QIIME2/Original_reads_16S_ITS_18S_negative_control/core/RepSeq_negative_control.qza
+#NEG_CONTROL=/scratch_vol0/fungi/valormicro/98_database_files/Negative_control_Sample_RepSeq_V4.qza
+NEG_CONTROL=/scratch_vol0/fungi/valormicro/99_contamination
+#NEG_CONTROL=/scratch_vol0/fungi/valormicro/05_QIIME2/Original_reads_16S_ITS_18S_negative_control/core/RepSeq_negative_control.qza
 
-TMPDIR=/home
+TMPDIR=/scratch_vol0
 
 # https://chmi-sops.github.io/mydoc_qiime2.html
 
@@ -30,7 +30,7 @@ eval "$(conda shell.bash hook)"
 conda activate qiime2-2021.4
 
 # I'm doing this step in order to deal the no space left in cluster :
-export TMPDIR='/home/fungi'
+export TMPDIR='/scratch_vol0/fungi'
 echo $TMPDIR
 
 # dada2_denoise :
