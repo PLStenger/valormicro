f8f4e83708e4c2bc75f86c16b0f8742a315639dd
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-05-07 19:53:10 +0200

    Update 09_qiime2_calculate_and_explore_diversity_metrics_PE.sh

--- 00_scripts/09_qiime2_calculate_and_explore_diversity_metrics_PE.sh
+++ 00_scripts/09_qiime2_calculate_and_explore_diversity_metrics_PE.sh
@@ -43,7 +43,7 @@ echo $TMPDIR
 qiime diversity core-metrics-phylogenetic \
        --i-phylogeny tree/rooted-tree.qza \
        --i-table core/Table.qza \
-       --p-sampling-depth 2929 \
+       --p-sampling-depth 5237 \
        --m-metadata-file $DATABASE/sample-metadata.tsv \
        --o-rarefied-table core/RarTable.qza \
        --o-observed-features-vector core/Vector-observed_asv.qza \
